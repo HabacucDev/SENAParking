a1a9c1757355c39c6fb33c12f3fcc1d2083f43f6
Habacuc <habacucdeveloper@gmail.com>
2025-04-05 15:20:20 -0500

    Enrutamiento de medios

---
+++ frontend/public/js/scriptsDOM.js
@@ -0,0 +1,12 @@
+        // Cargar el contenido del header desde un archivo HTML externo
+        fetch('/frontend/views/layouts/header.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('header-container').innerHTML = data;
+            })
+            .catch(error => console.error('Error al cargar el header:', error));
+
+        // Función para retroceder en el historial del navegador
+        function goBack() {
+            window.history.back();
+        }

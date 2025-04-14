6eaf1d4843bac1cc403a80191c65aefe3fbf8c2f
Jose Antonio Villaveces Rivera <javillaveces7@soy.sena.edu.co>
2025-04-13 19:40:40 -0500

    validacion de reg_accesos

---
+++ frontend/public/js/validacion_reg_accesos.js
@@ -0,0 +1,46 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+   nombre: /^[a-zA-ZÀ-ÿ]{4,80}$/, // Letras.
+   documento: /^\d{6,10}$/, // 6 a 10 digitos.
+}
+
+const campos = {
+   nombre: false,
+   documento: false
+}
+
+const validarformulario = (e) => {
+   switch (e.target.name) {
+      case "nombre":
+         validarcampo(expresiones.nombre, e.target, 'nombre');
+         break;
+      case "documento":
+         validarcampo(expresiones.documento, e.target, 'documento');
+         break;
+   }
+}
+
+const validarcampo = (expresiones, input, campo) => {
+   if (expresiones.test(input.value)) {
+      document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+      document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+      document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+      campos[campo] = true;
+   } else {
+      document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+      document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+      document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+      campos[campo] = false;
+   }
+}
+
+inputs.forEach((input)=>{
+   input.addEventListener('keyup', validarformulario);
+   input.addEventListener('blur', validarformulario);
+})
+
+formulario.addEventListener('submit', (e)=>{
+   e.preventDefault();
+});

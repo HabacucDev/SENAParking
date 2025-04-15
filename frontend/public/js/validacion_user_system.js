a861900a224d4a8c315055ecb1b5a4b51c0e6a0a
Juan-colmenares <119083166+Juan-colmenares@users.noreply.github.com>
2025-04-14 20:41:58 -0500

    correcciones

--- frontend/public/js/validacion_user_system.js
+++ frontend/public/js/validacion_user_system.js
@@ -7,7 +7,7 @@ const expresiones = {
    documento: /^\d{6,10}$/, // 6 a 10 digitos.
    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
    numero: /^\d{7,10}$/, // 7 a 10 numeros.
-   usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+   usuario: /^[a-zA-Z0-9]{4,16}$/, // Letras, numeros, guion y guion_bajo
    contrasena: /^.{4,12}$/ // 4 a 12 digitos.
 }
 
